3e6a61eb29fdc78a5ecf16fa61ade4d67d2cf682
Sebastian <sebastian.orozco2005@gmail.com>
2024-05-04 11:01:01 -0500

    Ecommerce Version 1.0

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const menu_mail = document.querySelector(".navbar-email");
-const menu_unfolded = document.querySelector(".desktop-menu")
+const menu_unfolded = document.querySelector(".desktop-menu");
+const menu_mobile_button = document.querySelector(".menu");
+const menu_mobile = document.querySelector(".mobile-menu")
 
-menu_mail.addEventListener('click',showContainer);
+menu_mail.addEventListener('click',function(){showContainer(menu_unfolded)});
+menu_mobile_button.addEventListener('click',function(){showContainer(menu_mobile)});
 
-function showContainer(){
+function showContainer(container){
     console.log("executed");
-
-    menu_unfolded.classList.toggle('inactive'); //El metodo toogle si esta clase la quita y si no esta la pone
+    container.classList.toggle('inactive'); //El metodo toogle si esta clase la quita y si no esta la pone
 
     //Manera mas larga de realizar el mismpo proceso que hace el metodo toogle
     
